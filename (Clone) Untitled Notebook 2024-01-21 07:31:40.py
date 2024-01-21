7e424a409d387b32e542471275fe017cc4e19a49
nalinisaraf <nalinisaraf>
2024-01-21 02:39:20 +0000

    modified

--- (Clone) Untitled Notebook 2024-01-21 07:31:40.py
+++ (Clone) Untitled Notebook 2024-01-21 07:31:40.py
@@ -1,7 +1,13 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC # Github connection
-# MAGIC ## add repo
+# MAGIC ## add repo 
+# MAGIC -- i want to change this
 # MAGIC ## set up integraton
 # MAGIC
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC adding changes
